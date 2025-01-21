4870a46df66c3a9caf68a33ff581cd92b4ace28d
Joiii <floresr.joey@pucp.edu.pe>
2025-01-20 23:12:21 -0500

    g

--- Spring Boot/cursoSpringBoot/cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/CustomerRestController.java
+++ Spring Boot/cursoSpringBoot/cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/CustomerRestController.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping("/clientes")
 public class CustomerRestController {
     private List<Customer> customers = new ArrayList<>(Arrays.asList(
             new Customer(123, "Gerardo Lopez", "gerardo", "123"),
@@ -17,13 +18,15 @@ public class CustomerRestController {
     ));
 
     //Endpoint de lista de clientes
-    @GetMapping("/clientes")
+    @GetMapping
+    //@RequestMapping(method = RequestMethod.GET)
     public List<Customer> getCustomers(){
         return customers;
     }
 
     //Endpoint de un cliente en especifico
-    @GetMapping("/clientes/{username}")
+    @GetMapping("/{username}")
+    //@RequestMapping(value = "/{username}", method = RequestMethod.GET)
     public Customer getCustomer(@PathVariable String username){
         for(Customer c: customers){
             if (c.getUsername().equalsIgnoreCase(username))
@@ -33,15 +36,17 @@ public class CustomerRestController {
         //Solo para devolver algo, no es buena practica por ahora. Mas adelante se hara manejo de excepciones.
     }
 
-    @PostMapping("/clientes")
+    @PostMapping
     //Se creo un metodo, pero hace falta decorar el metodo con PostMapping para que sea un endpoint
+    //@RequestMapping(method = RequestMethod.POST)
     public Customer postCustomer(@RequestBody Customer customer){
         customers.add(customer);
         return customer;
     }
 
-    @PutMapping("/clientes")
+    @PutMapping
     //Se encarga de actualizar los datos del cliente
+    //@RequestMapping(method = RequestMethod.PUT)
     public Customer putCustomer(@RequestBody Customer customer){
         for(Customer c: customers){
             if (c.getID() == customer.getID()){
@@ -54,11 +59,32 @@ public class CustomerRestController {
         return null;
     }
 
-    @DeleteMapping("/clientes/{idCustomer}")
+    @DeleteMapping("/{idCustomer}")
+    //@RequestMapping(value = "/{idCustomer}", method = RequestMethod.DELETE)
     public Customer deleteCustomer(@PathVariable int idCustomer){
         for(Customer c: customers){
             if (c.getID() == idCustomer){
                 customers.remove(c);
+                return c;
+            }
+        }
+        return null;
+    }
+
+    @PatchMapping
+    //@RequestMapping(method = RequestMethod.PATCH)
+    public Customer patchCustomer(@RequestBody Customer customer){
+        for(Customer c: customers){
+            if (c.getID() == customer.getID()){
+                if (customer.getName() != null){
+                    c.setName(customer.getName());
+                }
+                if (customer.getUsername() != null){
+                    c.setUsername(customer.getUsername());
+                }
+                if (customer.getPassword() != null){
+                    c.setPassword(customer.getPassword());
+                }
                 return c;
             }
         }
