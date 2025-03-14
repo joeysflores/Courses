7ff202dcf7739c2d4b8283cb4ade6a4ba7772564
Joiii <floresr.joey@pucp.edu.pe>
2025-03-14 11:53:57 -0500

    Cambios finales del carrito de compras

--- React-Udemy/Proyectos/guitarla/src/App.jsx
+++ React-Udemy/Proyectos/guitarla/src/App.jsx
@@ -7,17 +7,97 @@ function App() {
 
   //Nuestros datos desde un archivo para usarlo en el proyecto(es nuestra bd)
   const [data, setData] = useState(db)
-
   // //Otra forma: Si fuera desde una API, se usa useEffect
   // useEffect(() => {
   //   setData(db)
   // }, [])
 
-  console.log(data)
+  //Colocamos un valor inicial al carrito y lo pasamos al state para que se maneje mejor el localstorage
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
+
+  const [cart, setCart] = useState(initialCart)
+  const MAX_ITEMS = 5 //Codigo mas limpio
+  const MIN_ITEMS = 1
+
+  //Se lanza cuando el cart cambie
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
+
+  function addToCart(item){
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id)
+    console.log('Elementos existe?: ', itemExists)
+    //Validar si el elemento ya esta en el array
+    if (itemExists >= 0){
+      console.log('Existe...')
+      //Validar que no se exceda del limite
+      if (cart[itemExists].quantity >= MAX_ITEMS) return
+
+      const updatedCart = [...cart] //Pasamos una copia del cart
+      updatedCart[itemExists].quantity++
+      setCart(updatedCart)
+    }else{
+      console.log('No existe...Agregando')
+      item.quantity = 1
+      setCart([...cart, item]) //Agregar guitarras al arreglo cart
+    }
+  }
+  // console.log(data)
+
+  //Eliminar items del cart
+  function removeFromCart(id){
+    //Del arreglo, solo filtramos y quitamos la guitarra a eliminar 
+    setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
+  }
+
+  //Incrementar cantidades por item - hacer funcional al botoncito +
+  function increaseQuantity(id){
+    //Se puede hacer similar al addToCart, pero lo haremos con map
+    const updatedCart = cart.map(item => {
+      //Se le pone limite max de cantidad
+      if (item.id === id && item.quantity < MAX_ITEMS){
+        return{
+          ...item,
+          quantity: item.quantity + 1
+        }
+      }
+      return item
+    })
+    setCart(updatedCart)
+  }
+
+  //RETO 1: Decrementar cantidades por item
+  function decreaseQuantity(id){
+    const updatedCart = cart.map(item =>{
+      if (item.id === id && MIN_ITEMS < item.quantity){
+        return{
+          ...item, //copia del elemento
+          quantity: item.quantity - 1
+        }
+      }
+      return item
+    })
+    //Si es 0, quitarlo del carrito porque se le decrementara
+    setCart(updatedCart)
+  }
+
+  //Funcion limpiar carrito
+  function clearCart(){
+    setCart([])
+  }
 
   return (
     <>
-      <Header/>
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        clearCart={clearCart}
+      />
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
@@ -28,6 +108,8 @@ function App() {
               <Guitar
                 key={guitar.id} //Siempre se usa cuando se itere una lista
                 guitar={guitar}
+                setCart={setCart}
+                addToCart={addToCart}
               />
             )
           )}
--- React-Udemy/Proyectos/guitarla/src/components/Guitar.jsx
+++ React-Udemy/Proyectos/guitarla/src/components/Guitar.jsx
@@ -1,8 +1,9 @@
-export default function Guitar({guitar}){
+export default function Guitar({guitar, addToCart}){
   // console.log(guitar)
 
   //Opcion mas limpia, aunque puedes colocar directamente {guitar.name}
-  const {name, image, description, price} = guitar
+  const {id, name, image, description, price} = guitar
+
   return (
     <div className="col-md-6 col-lg-4 my-4 row align-items-center">
         <div className="col-4">
@@ -14,7 +15,8 @@ export default function Guitar({guitar}){
             <p className="fw-black text-primary fs-3">{price}</p>
             <button 
                 type="button"
-                className="btn btn-dark w-100 "
+                className="btn btn-dark w-100"
+                onClick={() => addToCart(guitar)}
             >Agregar al Carrito</button>
         </div>
     </div>
