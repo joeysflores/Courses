10ef0b78bf73328f969b7970c838f52543da62b1
Joiii <floresr.joey@pucp.edu.pe>
2025-10-28 15:44:44 -0500

    Hook useOrder ok

--- React-Udemy/Proyectos/calculadora_propinas/src/hooks/useOrder.ts
+++ React-Udemy/Proyectos/calculadora_propinas/src/hooks/useOrder.ts
@@ -5,8 +5,24 @@ export default function useOrder() {
   const [order, setOrder] = useState<OrderItem[]>([])
 
   const addItem = (item : MenuItem) => {
-    console.log(item)
+    // console.log(item)
+    const itemExist = order.find(orderItem => orderItem.id === item.id)
+    if (itemExist) {
+      // Si el item ya existe en el order, se actualiza su cantidad
+      const updatedOrder = order.map(orderItem => orderItem.id === item.id ? 
+        {...orderItem, quantity: orderItem.quantity + 1}: 
+        orderItem)
+        setOrder(updatedOrder)
+    } else{
+      // Se crea un nuevo objeto newItem que incluye todas las propiedades de item y agrega quantity con valor 1
+      // Esto con el fin de cumplir con el tipo OrderItem que requiere quantity
+      const newItem = {...item, quantity: 1}
+      
+      // Se crea un array nuevo agregando el elemento item al array order
+      setOrder([...order, newItem])
+    }
   }
+  console.log(order)
 
   return (
     addItem
